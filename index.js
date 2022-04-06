41b39c1bba3b9c80ac60c5c0c00a26fe832d96ea
evasion123 <tabajicaognjen@gmail.com>
2022-04-06 12:15:07 +0200

    first commit, added files

---
+++ index.js
@@ -0,0 +1,55 @@
+let myLeads = [];
+
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const emptyError = document.getElementById("empty-error");
+const clearBtn = document.getElementById("clear-btn");
+const doubleClick = document.getElementById("double-click");
+inputEl.value = "";
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+clearBtn.addEventListener("click", function () {
+  doubleClick.textContent = "Double click the button to clear leads";
+});
+
+clearBtn.addEventListener("dblclick", function () {
+  let proceed = confirm("Are you sure you want to clear the leads");
+  if (proceed) {
+    localStorage.clear();
+    myLeads = [];
+    renderLeads();
+  }
+});
+
+inputBtn.addEventListener("click", function () {
+  if (document.getElementById("input-el").value.length == 0) {
+    emptyError.textContent = "Empty input. Enter a lead before saving.";
+  } else {
+    myLeads.push(inputEl.value);
+    renderLeads();
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  }
+});
+function renderLeads() {
+  let listItems = "";
+  doubleClick.textContent = "";
+  emptyError.textContent = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+        <a target='_blank' href='https://${myLeads[i]}'>
+        https://${myLeads[i]}
+        </a>
+    </li>
+    `;
+  }
+  ulEl.innerHTML = listItems;
+}
